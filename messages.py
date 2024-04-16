2873e8d33709109d3a60f39b2daa2f259ff5c7c6
Karaatsar <162288076+Karaatsar@users.noreply.github.com>
2024-04-16 11:58:09 +0300

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,27 @@
+from db import db
+import users
+
+def get_list():
+    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id"
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def send(content):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = "INSERT INTO messages (content, user_id, sent_at) VALUES (:content, :user_id, NOW())"
+    db.session.execute(sql, {"content":content, "user_id":user_id})
+    db.session.commit()
+    return True
+
+def get_ketju_messages(ketju_name):
+    sql = "SELECT content, username FROM messages WHERE ketju = :ketju_name"
+    result = db.session.execute(sql, {"ketju_name": ketju_name})
+    messages = result.fetchall()
+    return messages
+
+def send_message(content, username, ketju_name):
+    sql= "INSERT INTO messages (content, username, ketju) VALUES (: content, :username, :ketju_name)"
+    db.session.execute(sql, {"content": content, "username": username, "ketju_name": ketju_name})
+    db.session.commit()
